177396c1736083729f9343f30f61a4b960ab1bb4
Aminata1011 <119609202+Aminata1011@users.noreply.github.com>
2023-08-06 16:21:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 let points=0;
+window.onload = function () {
+    let bar = document.querySelectorAll('.bar');
+    bar.forEach((progress) => {
+      let value = progress.getAttribute('data-value');
+      progress.style.width = `${value}%`;
+      let count = 0;
+      let progressAnimation = setInterval(() => {
+        count++;
+      if (count >= value) {
+        clearInterval(progressAnimation);
+      }
+    }, 15);
+    });
+  };
